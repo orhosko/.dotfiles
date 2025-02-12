9660e519fdaaf201e996fa98e973ef49ff95c145
orhosko <yilmazor21@itu.edu.tr>
2025-02-13 00:46:57 +0300

    Fix lsp-setup.lua

--- nvim/.config/nvim/lua/config/lsp-setup.lua
+++ nvim/.config/nvim/lua/config/lsp-setup.lua
@@ -1,8 +1,8 @@
-require('lspconfig').clangd.setup({
+require('lspconfig').clangd.setup{
   capabilities = { offsetEncoding = { 'utf-16' } },
   cmd = { "clangd", "--experimental-modules-support" },
   -- cmd = { "/home/berkay/Documents/projects/llvm-project/build/bin/clangd","--experimental-modules-support" }
-}
+} 
 
 require'lspconfig'.verible.setup{
   cmd = {'verible-verilog-ls', '--rules_config_search'},
