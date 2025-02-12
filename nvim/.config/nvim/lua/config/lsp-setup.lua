c28bd8a46c4672cee2e79bbd853ee7931c87f208
orhosko <yilmazor21@itu.edu.tr>
2025-02-13 00:38:25 +0300

    Update .gitignore

--- nvim/.config/nvim/lua/config/lsp-setup.lua
+++ nvim/.config/nvim/lua/config/lsp-setup.lua
@@ -1,9 +1,11 @@
 require('lspconfig').clangd.setup({
   capabilities = { offsetEncoding = { 'utf-16' } },
-})
+  cmd = { "clangd", "--experimental-modules-support" },
+  -- cmd = { "/home/berkay/Documents/projects/llvm-project/build/bin/clangd","--experimental-modules-support" }
+}
 
-require('lspconfig').verible.setup({
-  cmd = { 'verible-verilog-ls', '--rules_config_search' },
-})
+require'lspconfig'.verible.setup{
+  cmd = {'verible-verilog-ls', '--rules_config_search'},
+}
 
-require('lspconfig').lua_ls.setup({})
+-- require'lspconfig'.lua_ls.setup{}
