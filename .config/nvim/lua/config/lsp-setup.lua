d42bd16d0c9837abd7285a29dfbaab18712b2dbf
Orhan Berkay Yılmaz <diamond82berkay@gmail.com>
2025-05-01 15:32:42 +0300

    Add more language servers

--- .config/nvim/lua/config/lsp-setup.lua
+++ .config/nvim/lua/config/lsp-setup.lua
@@ -1,20 +1,21 @@
-require('lspconfig').clangd.setup{
+require('lspconfig').clangd.setup({
   capabilities = { offsetEncoding = { 'utf-16' } },
-  cmd = { "clangd", "--experimental-modules-support" },
+  cmd = { 'clangd', '--experimental-modules-support' },
   -- cmd = { "/home/berkay/Documents/projects/llvm-project/build/bin/clangd","--experimental-modules-support" }
-} 
+})
 
-require'lspconfig'.verible.setup{
-  cmd = {'verible-verilog-ls', '--rules_config_search'},
-}
+require('lspconfig').verible.setup({
+  cmd = { 'verible-verilog-ls', '--rules_config_search' },
+})
 
-require('lspconfig').pylsp.setup{}
+require('lspconfig').pylsp.setup({})
+require('lspconfig').zls.setup({})
 
-require('lspconfig').mlir_lsp_server.setup{}
+require('lspconfig').mlir_lsp_server.setup({})
 vim.filetype.add({
   extension = {
     mlir = 'mlir',
-  }
+  },
 })
 
--- require'lspconfig'.lua_ls.setup{}
+require('lspconfig').lua_ls.setup({})
