b1d7f4a368e85baa287e030eeff60ade314faa22
Orhan Berkay Yılmaz <diamond82berkay@gmail.com>
2025-06-09 21:23:05 +0300

    xserver removed from some names

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -47,8 +47,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -235,7 +235,7 @@
   environment.variables.EDITOR = "nvim";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  services.xserver.desktopManager.gnome = {
+  services.desktopManager.gnome = {
     extraGSettingsOverrides = ''
       # Change default background
       # [org.gnome.desktop.background]
