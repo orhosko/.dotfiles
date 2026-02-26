c4c1c87c7a931ac023ffb648230555dcabc86826
orhosko <yilmazorhanberkay@gmail.com>
2026-02-26 23:41:45 +0300

    misc

--- .config/nvim/lua/config/lsp-setup.lua
+++ .config/nvim/lua/config/lsp-setup.lua
@@ -1,22 +1,24 @@
 require('lspconfig').clangd.setup({
-  capabilities = { offsetEncoding = { 'utf-16' } },
-  cmd = { 'clangd', '--experimental-modules-support' },
-  -- cmd = { "/home/berkay/Documents/projects/llvm-project/build/bin/clangd","--experimental-modules-support" }
+  -- capabilities = { offsetEncoding = { 'utf-16' } },
+  -- cmd = { 'clangd', '--experimental-modules-support' },
+  -- -- cmd = { "/home/berkay/Documents/projects/llvm-project/build/bin/clangd","--experimental-modules-support" }
 })
 
-require('lspconfig').verible.setup({
-  cmd = { 'verible-verilog-ls', '--rules_config_search' },
-})
+-- require('lspconfig').verible.setup({
+--   cmd = { 'verible-verilog-ls', '--rules_config_search' },
+-- })
 
 require('lspconfig').pylsp.setup({})
-require('lspconfig').zls.setup({})
-require('lspconfig').gopls.setup({})
+-- require('lspconfig').zls.setup({})
+-- require('lspconfig').gopls.setup({})
 
-require('lspconfig').mlir_lsp_server.setup({})
-vim.filetype.add({
-  extension = {
-    mlir = 'mlir',
-  },
-})
+-- require('lspconfig').mlir_lsp_server.setup({})
+-- vim.filetype.add({
+--   extension = {
+--     mlir = 'mlir',
+--   },
+-- })
 
 require('lspconfig').lua_ls.setup({})
+require('lspconfig').ts_ls.setup({})
+require('lspconfig').ocamllsp.setup({})
